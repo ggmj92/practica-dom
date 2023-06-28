1f7aa86759c300bfdce756cdf428f9d3d163ae35
Gabriel GMJ <136244933+ggmj92@users.noreply.github.com>
2023-06-28 23:05:47 +0200

    avances?

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
 
 // WELCOME
-// const drawWelcome = ()=> {
-//     // Capturamos espacio donde vamos a pintar banner----------
+function drawWelcome() {
+    // Capturamos espacio donde vamos a pintar banner----------
 
-//     const cajaWelcome = document.querySelector('.welcome');
-
-//     // Creamos primer elemento del banner----------
-
-//     const welcomeText = document.createElement('H1');
-//     welcomeText.innerHTML = "Welcome";
-//     cajaWelcome.append(welcomeText);
-
-//     // Creamos imagen----------
-
-//     const welcomeImage = document.createElement('IMG');
-//     welcomeImage.src = ("assets/images/1.jpg");
-//     cajaWelcome.append(welcomeImage);
-
-// }
-const drawWelcome = () => {
-    // Capturamos espacio donde vamos a pintar banner
     const cajaWelcome = document.querySelector('.welcome');
-    const bannerImg = document.querySelector('.welcome img')
-    // Creamos primer elemento del banner
 
+    // Creamos primer elemento del banner----------
 
-    bannerImg.src = "assets/images/1.jpg";
+    const welcomeText = document.createElement('H1');
+    welcomeText.innerHTML = "Welcome";
+    cajaWelcome.append(welcomeText);
 
+    // Creamos imagen----------
 
+    const welcomeImage = document.createElement('IMG');
+    welcomeImage.src = ("assets/images/1.jpg");
+    cajaWelcome.append(welcomeImage);
 
 }
+
+// const drawWelcome = () => {
+//     // Capturamos espacio donde vamos a pintar banner
+//     const cajaWelcome = document.querySelector('.welcome');
+//     const bannerImg = document.querySelector('.welcome img')
+//     // Creamos primer elemento del banner
+
+
+//     bannerImg.src = "assets/images/1.jpg";
+
+
+
+// }
 
 
 // TRIPS
@@ -41,12 +42,12 @@ const drawWelcome = () => {
 //     cajaTrips.append(tripsText);
 // }
 
-document.createElement(".recommended"); {
-
+const cajaTrips = document.createElement(".recommended"); {
     const cajaTrips = document.createElement(".tripCards gridContainer"); {
         const photoTitles = document.createElement(".titles")
-        photoTitles.innerHTML = "";
+        photoTitles.innerHTML = "title";
         cajaTrips.append(photoTitles);
+        let images []
 
         const photoDescription = document.createElement(".description")
 
